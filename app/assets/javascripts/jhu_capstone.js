d0e05bf778da59c9aa96727fae3c079636d3f0aa
Sarmad Sabih <sam_sarmad@hotmail.com>
2017-07-21 17:35:29 +0500

    Week2 Sample Resource CRUD Controller Skeleton

--- app/assets/javascripts/jhu_capstone.js
+++ app/assets/javascripts/jhu_capstone.js
@@ -11,3 +11,4 @@
 
 //= require jhu_capstone/foos/foos.module
 //= require jhu_capstone/foos/foos.service
+//= require jhu_capstone/foos/foos.controller
---
+++ app/assets/javascripts/jhu_capstone/foos/foos.controller.js
@@ -0,0 +1,51 @@
+(function() {
+  "use strict";
+
+  angular
+    .module("jhu_capstone.foos")
+    .controller("jhu_capstone.foos.FoosController", FoosController);
+
+  FoosController.$inject = ["jhu_capstone.foos.Foo"];
+
+  function FoosController(Foo) {
+    var vm = this;
+    vm.foos;
+    vm.foo;
+
+    activate();
+    return;
+
+    function activate() {
+      newFoo();
+    };
+
+    function newFoo() {
+      vm.foo = new Foo();
+    };
+
+    function handleError(response) {
+      console.log(response);
+    };
+
+    function edit(object, index) {
+
+    };
+
+    function create() {
+
+    };
+
+    function update() {
+
+    };
+
+    function remove() {
+
+    };
+
+    function removeElement(elements, element) {
+
+    };
+
+  };
+})();
