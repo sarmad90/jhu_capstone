f92050bfe0ef63562345a54087f89c6a037de47d
Sarmad Sabih <sam_sarmad@hotmail.com>
2017-07-25 17:54:47 +0500

    Week2 Create Resource Instance and Select and Update/Delete Resource Instance

--- app/assets/javascripts/jhu_capstone/foos/foos.controller.js
+++ app/assets/javascripts/jhu_capstone/foos/foos.controller.js
@@ -11,6 +11,10 @@
     var vm = this;
     vm.foos;
     vm.foo;
+    vm.edit = edit;
+    vm.create = create;
+    vm.update = update;
+    vm.remove = remove;
 
     activate();
     return;
@@ -28,24 +32,49 @@
       console.log(response);
     };
 
-    function edit(object, index) {
-
+    function edit(object) {
+      console.log("Selected", object);
+      vm.foo = object;
     };
 
     function create() {
-
+      //console.log("creating foo", vm.foo);
+      vm.foo.$save()
+        .then(function(response){
+          console.log(response);
+          vm.foos.push(vm.foo);
+        })
+        .catch(handleError);
     };
 
     function update() {
-
+      vm.foo.$update()
+        .then(function(response){
+          console.log(response);
+        })
+        .catch(handleError);
     };
 
     function remove() {
-
+      vm.foo.$delete()
+        .then(function(response){
+          console.log(response);
+          // remove the element from the JS array
+          // removeElement(vm.foos, vm.foo);
+          vm.foos = Foo.query();
+          // replace edit area with prototype instance
+          newFoo();
+        })
+        .catch(handleError);
     };
 
     function removeElement(elements, element) {
-
+      for(var i=0; i<elements.length, i++) {
+        if (elements[i].id == element.id) {
+          elements.splice(i, 1);
+          break;
+        }
+      }
     };
 
   };
