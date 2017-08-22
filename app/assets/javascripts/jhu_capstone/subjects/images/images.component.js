add3012b0aa27a066534d559cff0b733a9d417cc
Sarmad Sabih <sam_sarmad@hotmail.com>
2017-08-22 16:02:29 +0500

    Week4 UI: Display Image Metadata

--- app/assets/javascripts/jhu_capstone/subjects/images/images.component.js
+++ app/assets/javascripts/jhu_capstone/subjects/images/images.component.js
@@ -6,12 +6,22 @@
     .component("sdImageSelector", {
       templateUrl: imageSelectorTemplateUrl,
       controller: ImageSelectorController,
-      //bindings: {},
+      bindings: {},
+    })
+    .component("sdImageEditor", {
+      templateUrl: imageEditorTemplateUrl,
+      controller: ImageEditorController,
+      bindings: {},
     });
 
   imageSelectorTemplateUrl.$inject = ["jhu_capstone.config.APP_CONFIG"];
   function imageSelectorTemplateUrl(APP_CONFIG) {
     return APP_CONFIG.image_selector_html;
+  }
+
+  imageEditorTemplateUrl.$inject = ["jhu_capstone.config.APP_CONFIG"];
+  function imageEditorTemplateUrl(APP_CONFIG) {
+    return APP_CONFIG.image_editor_html;
   }
 
   ImageSelectorController.$inject = ["$scope", "$stateParams", "jhu_capstone.subjects.Image"];
@@ -26,5 +36,25 @@
     }
     return;
     /////
+  }
+
+  ImageEditorController.$inject = ["$scope", "$stateParams", "jhu_capstone.subjects.Image"];
+  function ImageEditorController($scope, $stateParams, Image) {
+    var vm = this;
+    vm.$onInit = function() {
+      console.log("ImageEditorController", $scope);
+      if ($stateParams.id) {
+        vm.item = Image.get({id: $stateParams.id});
+      } else {
+        newResource();
+      }
+
+    }
+    return;
+    /////
+    function newResource() {
+      vm.item = new Image();
+      return vm.item;
+    }
   }
 })();
